2f92a17c1e0285f11154bf901b3a12a1167373ad
Anaada <anaada2015@gmail.com>
2021-08-31 19:45:26 +0600

    Updated

--- client/components/tournaments/tournamentCard.js
+++ client/components/tournaments/tournamentCard.js
@@ -1,5 +1,8 @@
+import moment from 'moment';
 import React from "react";
-import "./tournamentCard.module.css";
+// import "./tournamentCard.module.css";
+//disable es-lint next line
+import Styles from './tournamentCard.module.css';
 export default function TournamentCard({
   mode,
   prize,
@@ -8,17 +11,27 @@ export default function TournamentCard({
   createdAt,
   isCenterdCard,
 }) {
+  // {`${Styles.ananda} ananda`}
+  // console.log(`createdAt`, createdAt)
+  const starStyle = {
+    "background": "#ebe6fd",
+    "border-radius": "20px",
+    "padding": "3px",
+    "max-width": "13rem",
+    "width": "100%",
+    "margin": "0 auto",
+    "fontSize": "14px",
+  }
   return (
     <div className="flex flex-col" md="4">
       <div
         style={{
-          height: isCenterdCard || "670px",
-          marginTop: isCenterdCard || "-40px",
+          height: isCenterdCard || "575px",
+          marginTop: isCenterdCard || "-6px",
         }}
-        className="tournamentCard"
-      >
+        className="tournamentCard relative bg-gray-50 rounded mt-5 mx-3">
         <img
-          className="img"
+          className={`${Styles.icon} img absolute`}
           src={
             mode === "solo"
               ? "/images/single.svg"
@@ -28,51 +41,51 @@ export default function TournamentCard({
           }
           alt="single icon"
         ></img>
-        <div className="title">
+        <div className="title  text-center font-medium mt-3 text-3xl ">
           Free <span style={{ textTransform: "capitalize" }}>{`${mode}s`}</span>
         </div>
-        <div className="text">{createdAt}</div>
-        <div className="tag">
-          <img src="/images/star.png" alt="star" className="starIcon"></img>
+        <div className="text-center my-2 text-gray-300">{moment(startTime).format('LLLL')}</div>
+        <div className="flex justify-center mt-4 bg-gray-50" style={starStyle}>
+          <img src="/images/star.png" style={{height:"13px"}}alt="star" className="starIcon mt-1 mr-1"></img>
           <span>200+ teams expected</span>
         </div>
-        <div className="prizeText">${prize} Prize</div>
-        <div className="prizesubText">PER DIVISION</div>
-        <div className="items mt-4">
-          <span>Mode</span>
+        <div className="prizeText text-center my-2 font-bold text-4xl">${prize} Prize</div>
+        <div className="prizesubText text-center text-gray-400">PER DIVISION</div>
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Mode</span>
           <span
-            className="poppinsBd"
+            className="poppinsBd font-bold"
             style={{ textTransform: "capitalize" }}
           >{`${mode}s`}</span>
         </div>
-        <div className="items">
-          <span>Length</span>
-          <span className="poppinsBd">1H:30M</span>
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Length</span>
+          <span className="poppinsBd font-bold">1H:30M</span>
         </div>
-        <div className="items">
-          <span>Start Time</span>
-          <span className="poppinsBd">
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Start Time</span>
+          <span className="poppinsBd font-bold">
             {/*{`${formatDate(parseISO(startTime), "p")} PDT`}*/}
             startTime
           </span>
         </div>
-        <div className="items">
-          <span>Scoring</span>
-          <span className="poppinsBd">{`${scoring} ${
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Scoring</span>
+          <span className="poppinsBd font-bold">{`${scoring} ${
             mode === "solo" ? "Players" : "Teams"
           }`}</span>
         </div>
-        <div className="items">
-          <span>Teams / Divison</span>
-          <span className="poppinsBd">20</span>
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Teams / Divison</span>
+          <span className="poppinsBd font-bold	">20</span>
         </div>
-        <div className="items">
-          <span>Cost/Player</span>
-          <span className="poppinsBd textGreen">Free Entry</span>
+        <div className="items flex justify-between mt-4 mx-3">
+          <span className="font-medium">Cost/Player</span>
+          <span className="poppinsBd font-bold" style={{color:"#8de20a"}}>Free Entry</span>
         </div>
         <button
-          className="btn registerButton"
-          style={{ marginTop: isCenterdCard || "65px" }}
+          className="btn registerButton text-center my-3"
+          style={{ marginTop: isCenterdCard || "65px" ,margin:"12px auto",display:"block"}}
         >
           REGISTER NOW
         </button>
--- client/pages/tournaments/index.js
+++ client/pages/tournaments/index.js
@@ -4,12 +4,12 @@ import Header from "../../components/layout/header";
 // import { Col, Container, Row } from "reactstrap";
 import TournamentCard from "../../components/tournaments/tournamentCard";
 import { ALL_TOURNAMENTS } from "../../services/graphQL/queries/tournament";
-import "./tournament.module.css";
+import Styles from "./tournament.module.css";
 export default function Index() {
   // fetching data
   const { data, error, loading } = useQuery(ALL_TOURNAMENTS);
 
-  console.log(data);
+  // console.log(data);
   // main return
   return (
     <div className="flex flex-col w-full bg-indigo-900 tournament"
@@ -31,21 +31,167 @@ export default function Index() {
         <>
           <div className="flex w-full mt-5">
             <div className="flex flex-col">
-              <div className="totalTitle"> {data?.tournamentMany.length || 0} tournament</div>
+              <div className="totalTitle text-gray-50"> {data?.tournamentMany.length || 0} tournament</div>
             </div>
           </div>
-          <div className="flex flex-3 w-full mt-5">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 my-3">
             {data?.tournamentMany.map((item, index) => {
               const isCenterdCard = index % 2 === 0;
+              // console.log(`item`, item)
               return (
                 <TournamentCard
                   key={item._id}
                   isCenterdCard={isCenterdCard}
                   {...item}
                 />
+              
               );
             })}
           </div>
+          <div className={`${Styles.howItWorks} text-lg text-white font-bold bg-indigo-400 mt-4` }>
+            <h1 className="ml-5">How it works</h1>
+            <div className="flex justify-around mt-3">   
+              <div>
+                <span className="text-7xl	mx-5 my-3">1</span>
+                <h3 className={`mx-3 mt-4`}>Register For Heat</h3>
+                <div className={`${Styles.buttomBorder}`}></div>
+                <div className="description mx-3 text-base font-normal">
+                <p>Select the heat you want to compete in. You are able to register in multiple heats</p>
+                </div>
+                {/*  */}
+              </div>
+              <div>
+                <span className="text-7xl mx-5	my-3">2</span>
+                <h3 className={` mx-3 mt-4 `}>Compete</h3>
+                <div className={`${Styles.buttomBorder}`}></div>
+                <div className="description mx-3 text-base font-normal">
+                <p>Select the heat you want to compete in. You are able to register in multiple heats</p>
+                </div>
+              </div>
+              <div>
+                <span className="text-7xl mx-5 my-3">3</span>
+                <h3 className={`mx-3 mt-4`}>Win the Grand Prize</h3>
+                <div className={`${Styles.buttomBorder}`}></div>
+                <div className="description mx-3 text-base font-normal	">
+                <p>Select the heat you want to compete in. You are able to register in multiple heats</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={`${Styles.liveScoring} bg-indigo-200 text-center`}>
+              <h1 className="text-3xl font-bold">Live Scoring</h1>
+              <p className="text-base text-sm">Track how your team is doing in real time</p>
+              <div className={`${Styles.tableDiv} bg-indigo-100`}>
+                <table className={`${Styles.tables} mt-5`}>
+                  <tr className="text-purple-600	">
+                    <th className="text-xs" colspan="1">#</th>
+                    <th className="text-xs">Team Name</th>
+                    <th className="text-xs">Best 2 Placement Kills</th>
+                    <th className="text-xs">Best 2 kills</th>
+                    <th className="text-xs">Total Points</th>
+                    <th className="text-xs">Total Victory Royal</th>
+                    <th className="text-xs">Total Number of Kills</th>
+                    <th className="text-xs">Total Games Played</th>
+                  </tr>
+                  <tr className="text-indigo-800">
+                    <td>1</td>
+                    <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="bg-blue-900 text-gray-50">
+                  <td>2</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="bg-indigo-400 text-gray-50">
+                  <td>Game 1</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr>
+                  <td className="text-indigo-800">Game 2</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="text-indigo-800">
+                  <td>Game 3</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="bg-indigo-400 text-gray-50">
+                  <td>Game 4</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="bg-indigo-100 text-indigo-800">
+                  <td >Game 4</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="text-indigo-800">
+                  <td>3</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                  <tr className="text-indigo-800">
+                  <td>4</td>
+                  <td>Fish Dance</td>
+                    <td>2</td>
+                    <td>7</td>
+                    <td>4</td>
+                    <td>66</td>
+                    <td>28</td>
+                    <td>94</td>
+                  </tr>
+                 </table>
+              </div>
+
+          </div>
+          
+          
+
+
         </>
       )}
     </div>
